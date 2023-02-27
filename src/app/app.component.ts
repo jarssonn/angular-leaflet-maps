8556f8cc9eef4bbdf2a03b34810af326e8362ee2
Jaroslaw Gliwski <jaroslaw.gliwski@gmail.com>
2023-02-27 21:28:20 +0100

    Add GeoJSON with properties to map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,96 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  mapCenter = { lng: 20.17579828033186, lat: 50.43817144362623 }
+  
+  inputData: GeoJSON.FeatureCollection = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "tooltip": "Point A",
+          "description": "description Point A",
+        },
+        "geometry": {
+          "coordinates": [
+            20.695046327909182,
+            50.17479144407329
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "tooltip": "Point B",
+          "description": "description Point B"
+        },
+        "geometry": {
+          "coordinates": [
+            20.197143044413565,
+            50.074256632096024
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "tooltip": "Point C",
+          "description": "description Point C",
+        },
+        "geometry": {
+          "coordinates": [
+            20.36790115706583,
+            50.682243960994896
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "tooltip": "Point D",
+          "description": "description Point D",
+        },
+        "geometry": {
+          "coordinates": [
+            20.17579828033186,
+            50.43817144362623
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "tooltip": "Line A",
+          "description": "description Line A",
+        },
+        "geometry": {
+          "coordinates": [
+            [
+              20.69714708640683,
+              50.17433786141996
+            ],
+            [
+              20.20153607583316,
+              50.07378417621186
+            ],
+            [
+              20.172259476052375,
+              50.442780794798864
+            ],
+            [
+              20.548672901805162,
+              50.27468037680964
+            ]
+          ],
+          "type": "LineString"
+        }
+      }
+    ]
+  }
+
 }
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import * as Leaflet from 'leaflet'; 
+import { Component, Input } from '@angular/core';
+import * as Leaflet from 'leaflet';
 
 Leaflet.Icon.Default.imagePath = 'assets/';
 
@@ -9,51 +9,39 @@ Leaflet.Icon.Default.imagePath = 'assets/';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent {
+
+  @Input()
+  featureColletion!: GeoJSON.FeatureCollection;
+
   map!: Leaflet.Map;
+
   markers: Leaflet.Marker[] = [];
+
   options = {
     layers: [
       Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
       })
     ],
-    zoom: 16,
-    center: { lat: 28.626137, lng: 79.821603 }
+    zoom: 8,
+    center: { lng: 20.17579828033186, lat: 50.43817144362623 }
   }
 
-    initMarkers() {
-    const initialMarkers = [
-      {
-        position: { lat: 28.625485, lng: 79.821091 },
-        draggable: true
-      },
-      {
-        position: { lat: 28.625293, lng: 79.817926 },
-        draggable: false
-      },
-      {
-        position: { lat: 28.625182, lng: 79.81464 },
-        draggable: true
-      }
-    ];
-    for (let index = 0; index < initialMarkers.length; index++) {
-      const data = initialMarkers[index];
-      const marker = this.generateMarker(data, index);
-      marker.addTo(this.map).bindPopup(`<b>${data.position.lat},  ${data.position.lng}</b>`);
-      this.map.panTo(data.position);
-      this.markers.push(marker)
+  initializeData() {
+    const features = this.featureColletion.features;
+    for (let i = 0; i < features.length; i++) {
+      const feature: GeoJSON.Feature = features[i];
+      let properties = feature.properties ?? {};
+
+      Leaflet.geoJSON(feature).addTo(this.map)
+       .bindTooltip(properties['tooltip'])
+       .bindPopup(`<b>${properties['description']}</b>`)
     }
-  }
-
-  generateMarker(data: any, index: number) {
-    return Leaflet.marker(data.position, { draggable: data.draggable })
-      .on('click', (event) => this.markerClicked(event, index))
-      .on('dragend', (event) => this.markerDragEnd(event, index));
   }
 
   onMapReady($event: Leaflet.Map) {
     this.map = $event;
-    this.initMarkers();
+    this.initializeData();
   }
 
   mapClicked($event: any) {
@@ -66,6 +54,19 @@ export class MapComponent {
 
   markerDragEnd($event: any, index: number) {
     console.log($event.target.getLatLng());
-  } 
-  
+  }
+
+  // example of creating draggable marker
+  generateMarker(data: any, index: number) {
+    return Leaflet.marker(data.position, { draggable: data.draggable })
+      .on('click', (event) => this.markerClicked(event, index))
+      .on('dragend', (event) => this.markerDragEnd(event, index));
+  }
+
+  // example of creating polyline
+  generatePolyline() {
+    var polyline = Leaflet.polyline([{ lat: 28.625485, lng: 79.821091 }, { lat: 28.625293, lng: 79.817926 }, { lat: 28.625182, lng: 79.81464 }]);
+    polyline.addTo(this.map);
+  }
+
 }
